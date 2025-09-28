cf9c4d22aafd1e616bc3fd042dd23aba76fdbea5
zainasaleh2010-cmd <zaina.saleh2010@gmail.com>
2025-09-28 17:10:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,14 @@
 import streamlit as st
 import pickle
 
-# -----------------------------
-# تحميل الموديل
-# -----------------------------
 with open(autism_app.ipynb) as f:
     model = pickle.load(f)
 
-# -----------------------------
-# عنوان الصفحة
-# -----------------------------
 st.set_page_config(page_title="Autism Prediction App", page_icon="🧠", layout="centered")
 
 st.title("🧠 Autism Prediction System")
 st.write("ادخل بيانات الطفل عشان نعمل التنبؤ 👇")
 
-# -----------------------------
-# إدخالات المستخدم
-# -----------------------------
 gender = st.selectbox("جنس الطفل:", ["Male", "Female"])
 age_child = st.number_input("عمر الطفل:", min_value=0, max_value=18, value=5)
 mother_age = st.number_input("عمر الأم:", min_value=15, max_value=60, value=30)
@@ -32,9 +23,6 @@ speech_delay = st.selectbox("هل يوجد تأخر في الكلام؟", ["No",
 eye_contact = st.selectbox("هل الطفل يتجنب التواصل البصري؟", ["No", "Yes"])
 social_interaction = st.selectbox("هل الطفل يعاني من صعوبات اجتماعية؟", ["No", "Yes"])
 
-# -----------------------------
-# تحويل الخيارات لأرقام (نفس الكود اللي استعملتيه وقت التدريب)
-# -----------------------------
 gender_num = 1 if gender == "Male" else 0
 smoke_num = 1 if smoke == "Yes" else 0
 family_num = 1 if family_history == "Yes" else 0
@@ -44,17 +32,14 @@ speech_num = 1 if speech_delay == "Yes" else 0
 eye_num = 1 if eye_contact == "Yes" else 0
 social_num = 1 if social_interaction == "Yes" else 0
 
-# مصفوفة الإدخال
 data = [[
     gender_num, age_child, mother_age, father_age,
     smoke_num, family_num, premature_num, complications_num,
     speech_num, eye_num, social_num
 ]]
 
-# -----------------------------
-# التنبؤ
-# -----------------------------
-if st.button("🔮 اعمل التوقع"):
+
+if st.button(" اعمل التوقع"):
     prediction = model.predict(data)
 
     if prediction[0] == 1:
